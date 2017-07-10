8606c3109c6b023439b27de4006a6600afe19383
nir <nirnovo@qq.com>
2017-07-11 02:53:50 +0800

    [Add]add navbar and style

--- www.py
+++ www.py
@@ -18,6 +18,7 @@ def getfromdb(username):
         times = conn.hget(username, "time")
     else:
         times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        times = times.encode(encoding='utf_8', errors='strict')
     return content, times
 
 
